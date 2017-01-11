f7df60cba8a075dbef0033de97f8f82cf39de828
Ian Ibbotson <ian.ibbotson@k-int.com>
2017-01-11 16:12:48 +0000

    Work on auth

--- sstda/grails-app/controllers/sstda/TrafficController.groovy
+++ sstda/grails-app/controllers/sstda/TrafficController.groovy
@@ -1,7 +1,10 @@
 package sstda
 
+import grails.plugin.springsecurity.annotation.Secured
+
 class TrafficController {
 
+  @Secured(['ROLE_USER', 'IS_AUTHENTICATED_FULLY'])
   def index() { 
     if ( request.method == 'POST' ) {
       log.debug("Handle post");
